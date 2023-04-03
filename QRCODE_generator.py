cd2cd213c5ee6b79e57942e91cec33e6aa5e3580
Hubylex <99949840+Hubylex@users.noreply.github.com>
2023-04-03 22:16:55 +0000

    Update QRCODE_generator.py

--- QRCODE_generator.py
+++ QRCODE_generator.py
@@ -1,21 +1,21 @@
 import PySimpleGUI as sg
 import qrcode 
 layout = [
+    [sg.Text('Enter the text or URL to convert to QR code:')],
     [sg.Input(key='data')],
-    [sg.Button('GENERATE'),sg.Button('CLOSE')],
+    [sg.Button('GENERATE'),sg.Button('EXIT')],
     [sg.Image(key='output')]
 
 ]
-window = sg.Window('QRCODE',layout)
+window = sg.Window('QRCODE GENERATOR',layout)
 
 while True:
     event,values = window.read()
-    if event == sg.WIN_CLOSED or event == 'CLOSE':
+    if event == sg.WIN_CLOSED or event == 'EXIT':
        break
     elif event == 'GENERATE':
         qc = qrcode.make(values['data'])
         qc.save('qr_code.png')
         window['output'].update('qr_code.png')
 window.close()
-
 
